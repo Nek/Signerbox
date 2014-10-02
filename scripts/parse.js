ba2a5276ab8cf6a542b8107ac03822bb90f30618
Nikita Dudnik <nikdudnik@gmail.com>
2014-10-02 15:29:46 +0300

    Add parse function mock.

---
+++ scripts/parse.js
@@ -0,0 +1,18 @@
+module.exports = function(f) {
+	/*
+	returns "X.509" || "PRIVKEYS" || "IIT"
+	certificate with credentials
+	non-coded private keys
+	encoded private keys
+	{
+		format: "PRIVKEYS",
+		keys: [1234, 123213] // 1 or 2 elements
+	}
+
+	*/
+	return {
+		format: "X.509", // || "PRIVKEYS" || "IIT"
+		name: "Vasya Pupkin",
+		pub_key: "12FA3E"
+	}
+}
